f9aa71e7186568c8991f134839afa0a9dddc8c74
paddy911 <paddy911@gmx.de>
2026-03-12 08:10:44 +0100

    fix: build.rs Typfehler E0283 behoben

--- build.rs
+++ build.rs
@@ -4,22 +4,24 @@
 use cxx_qt_build::{CxxQtBuilder, QmlModule};
 
 fn main() {
+    // QML-Dateien als &str-Slice (expliziter Typ löst E0283)
+    let qml_files: &[&str] = &[
+        "qml/main.qml",
+        "qml/EingabeSeite.qml",
+        "qml/DatenbankSeite.qml",
+    ];
+
     CxxQtBuilder::new()
         // Qt-Module die benötigt werden
         .qt_module("Quick")
         .qt_module("QuickControls2")
         .qt_module("Sql")
-        // QML-Modul registrieren
-        .qml_module(QmlModule {
-            uri: "com.verbrauchsmanager",
+        // QML-Modul registrieren – Typen explizit angegeben: <&str, &str>
+        .qml_module(QmlModule::<&str, &str> {
+            uri:           "com.verbrauchsmanager",
             version_major: 1,
             version_minor: 0,
-            qml_files: &[
-                "qml/main.qml",
-                "qml/EingabeSeite.qml",
-                "qml/TabelleSeite.qml",
-                "qml/DatenbankSeite.qml",
-            ],
+            qml_files,
             ..Default::default()
         })
         // Bridge-Datei: enthält #[cxx_qt::bridge]
